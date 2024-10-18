e2c4227bd9dbdd8bd007f2887b9f3aa32ed87e57
sipto99 <shsipto99@gmail.com>
2024-10-18 17:02:34 +0600

    update and delete done

--- index.js
+++ index.js
@@ -25,31 +25,55 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const touristsSpotCollection= client.db('spotDB').collection('spot')
-    const userCollection= client.db('UsersDB').collection('user')
+    const touristsSpotCollection = client.db('spotDB').collection('spot')
+    const userCollection = client.db('UsersDB').collection('user')
 
-    app.get('/tourists-spots', async(req,res)=>{
+    app.get('/tourists-spots', async (req, res) => {
       const cursor = touristsSpotCollection.find()
       const result = await cursor.toArray()
       res.send(result)
     })
-    app.get('/tourists-spots/:id', async(req,res)=>{
+    app.get('/tourists-spots/:id', async (req, res) => {
       const id = req.params.id
-      const query = {_id:new ObjectId(id)}
-      const result = await  touristsSpotCollection.findOne(query)
+      const query = { _id: new ObjectId(id) }
+      const result = await touristsSpotCollection.findOne(query)
       res.send(result)
     })
 
-    app.post('/tourists-spots', async(req,res)=>{
+    app.post('/tourists-spots', async (req, res) => {
       const spot = req.body
       const result = await touristsSpotCollection.insertOne(spot)
       res.send(result)
     })
 
-    app.delete('/tourists-spots/:id', async(req,res)=>{
+    app.put('/tourists-spots/:id', async (req, res) => {
       const id = req.params.id
-      const query = {_id:new ObjectId(id)}
-      const result = await  touristsSpotCollection.deleteOne(query)
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const UpdatedSpotData = req.body
+      const spot = {
+        $set: {
+          name: UpdatedSpotData.name,
+          email: UpdatedSpotData.email,
+          image: UpdatedSpotData.image,
+          spotName: UpdatedSpotData.spotName,
+          countryName: UpdatedSpotData.countryName,
+          location: UpdatedSpotData.location,
+          avgCost: UpdatedSpotData.avgCost,
+          seasonality: UpdatedSpotData.seasonality,
+          travelTime: UpdatedSpotData.travelTime,
+          totalVisitorsPerYear: UpdatedSpotData.totalVisitorsPerYear,
+          description: UpdatedSpotData.description,
+        }
+      }
+      const result = await touristsSpotCollection.updateOne(filter, spot, options)
+      res.send(result)
+    })
+
+    app.delete('/tourists-spots/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await touristsSpotCollection.deleteOne(query)
     })
 
     // Send a ping to confirm a successful connection
@@ -63,8 +87,8 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Torists Spot server is running')
+  res.send('Torists Spot server is running')
 })
-app.listen(port, ()=>{
-    console.log(`Tourism Server is running on port: ${port}`)
+app.listen(port, () => {
+  console.log(`Tourism Server is running on port: ${port}`)
 })
